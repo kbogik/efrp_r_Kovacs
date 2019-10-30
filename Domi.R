20215dc67f2c75235688c2de84af64c419c3a83f
kbogik <kbogi1998@yahoo.de>
2019-10-30 21:06:36 +0100

    Update Domi.R

--- Domi.R
+++ Domi.R
@@ -23,7 +23,7 @@ correl=cor(wti[sapply( wti, is.numeric)])
 # window_length=2000, erre kellene hibauzenet
 #nem tudom az elejerol inditani az ablakot :(, azert van -10, 6 listaelembol áll a ccf eredmenye (erre 201-tol indulo listat ad, vagyis azelso 201 elem 0)
 
-ij_ccf<-list(0,0,0,0,0,0)
+ij_ccf<-list()
 cl_length<-nrow(wti)
 cl_number<-ncol(wti)
 window_length=2000
@@ -31,16 +31,26 @@ i=2
 j=window_length
 k=3
 
+#ha még csak erre futtatjátok, akkor működik
+cl_ccf<-list()
+j=2551
+while(j <= cl_length){
+  cl_ccf[[j]]<-(ccf(wti$CL1[j-(1):j], wti$CL2[j-(1):j]))
+  j <- j + 1
+}
+
+#a ciklust am elrontotam sorry
 while (i<=(cl_number)){
  while(j <= cl_length){
   while(k <= cl_number){
-    ij_ccf[j]<-(ccf(wti[j-(j-10):j,i], wti[j-(j-10):j,k]))
+    ij_ccf[[j]]<-(ccf(wti[j-(j-10):j,i], wti[j-(j-10):j,k]))
     k<-k+1
   }
   j <- j + 1
  i <- i + 1
 }
 }
+
 
 ##
 library(ggplot2)
