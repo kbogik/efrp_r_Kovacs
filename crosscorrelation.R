1f7de8d15ebd63fd2317ee1f96064f3983e45069
DomKiss <kissdomonkos1@gmail.com>
2019-10-31 20:29:05 +0000

    beleraktam a parameters-t az intervallum függvénybe

--- crosscorrelation.R
+++ crosscorrelation.R
@@ -15,12 +15,18 @@ wti<-readxl::read_excel(paste(dirname(getActiveDocumentContext()$path), "/WTI2.x
 parameters <- c("Date","CL1", "CL2","CL3","CL4","CL5","CL6","CL7","CL8","CL9","CL10","CL11","CL12","CL13","CL14","CL15","CL16","CL17","CL18","CL19","CL20","CL21","CL22","CL23","CL24")
 
 wti$Date = as.Date(wti$Date, format="%m/%d %M:%S")
-intervallum <- function(x,y){wti[wti$Date >= x & wti$Date <= y,]}
+intervallum <- function(x,y){wti[wti$Date >= x & wti$Date <= y,] %>%
+          dplyr::as_tibble() %>%
+          dplyr::select(parameters)
+
+  }
+                
 
 DATE1 <- as.Date("2011-04-29")
 DATE2 <- as.Date("2012-05-04")
 
 usedata <- intervallum(DATE1,DATE2) 
+          
 
 #usedata<- wti %>%
             #dpylr::as_tibble() %>%
